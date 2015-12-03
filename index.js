b894d83e64ae7cf2460e4d0aa42bb6fdcd202dc9
Taylor King <kingtb@appstate.edu>
2015-12-02 22:00:55 -0500

    add actual function

---
+++ index.js
@@ -0,0 +1,43 @@
+var uuid = require('node-uuid');
+var aws = require('aws-sdk');
+var dynamo = new aws.DynamoDB();
+exports.handler = function(event, context) {
+	var messId = uuid.v4();
+	var chatId = event.message.chat.id;
+	var text = event.message.text;
+	var chat = event.message.chat.title;
+	var author = event.message.from.username;
+	var params = {
+		TableName: "logs",
+		Item: {
+			"id":{
+				"S":messId
+			}, 
+			"chat_id": {
+				"N":chatId
+			},
+			"chat_name": {
+				"S":chat
+			},
+			"author":{
+				"S":author
+			},
+			"readable_date": {
+				"S":Date().toString()
+			},
+			"text": {
+				"S":text
+			},
+			"time": {
+				"N":Date.now()
+			}
+		}	
+	}
+	dynamo.putItem(params, function(err, data) {
+		if(err) {
+			context.fail(err);
+			return;
+		}
+		context.succeed(data);
+	});
+};
